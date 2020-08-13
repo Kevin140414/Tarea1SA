8b0c1ac62d5b520a0c4ddf842925c04043bbfded
Kevin Oswaldo Mejia Lemus <keosme@gmail.com>
2020-08-13 01:01:44 -0600

    update readme

--- Practica2_Parte1/SATarea1.php
+++ Practica2_Parte1/SATarea1.php
@@ -1,20 +1,32 @@
 <?php
 
 /*
+*   Tipo de concesión de credenciales de cliente
+*   utilizando el client_id y client_secret
+*/
+$client_id = 'sa';
+$client_secret = 'fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745';
+$grant_type = 'client_credentials';
+
+/*
 *	Funcion encargada de crear un contacto
 *	@name: nombre del contacto a crear
 */
-function create_contact($name)
+function create_contact($name, $token)
 {
     $contact = [
         'name' => $name,
     ];
 
+    $authorization = "Authorization: Bearer ".$token; // Prepare the authorisation token
+
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://api.softwareavanzado.world/index.php?option=com_contact&webserviceVersion=1.0.0&webserviceClient=administrator&list[limitstart]=10&api=Hal");
+    curl_setopt($ch, CURLOPT_URL, "https://api.softwareavanzado.world/index.php?option=com_contact&webserviceVersion=1.0.0&webserviceClient=administrator&api=Hal");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $contact);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($contact));
+    curl_setopt($ch, CURLOPT_POST, 1);
     $response = curl_exec($ch);
     curl_close($ch);
 
@@ -26,12 +38,12 @@ function create_contact($name)
 *	Funcion encargada de crear los contactos
 *	@array: arreglo utilizado para
 */
-function create_contacts($array)
+function create_contacts($array, $token)
 {
     foreach ($array as $name) {
-        $result = create_contact($name);
+        $result = create_contact($name, $token);
         if ($result['result'] == 1) {
-            echo "El contacto: {$name} fue creado exitosamente con ID: {$result['id']}\n";
+            echo "El contacto: {$name} fue creado exitosamente con ID: {$result['id']} <br>\n";
         }
     }
 }
@@ -40,33 +52,20 @@ function create_contacts($array)
 *	Obtiene la lista de contactos
 *	@filter_string: string utilizado para filtrar los datos
 */
-function list_contact_filter($filter_string)
+function list_contact_filter($filter_string, $token)
 {
     $filter_string = urlencode($filter_string);
-    $authorization = "Authorization: Bearer fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745";
-
+    $authorization = "Authorization: Bearer ".$token; // Prepare the authorisation token
+    
     $ch = curl_init();
-    $headers = array(
-        'Accept' => 'application/json',
-        'Accept-Language' => 'en_US',
-        "grant_type=client_credentials"
-    );
-
-    $data =
-    'client_id=' . 'sa' . '&' .
-    'client_secret=' . ' fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745' . '&' .
-    "grant_type=client_credentials";
-
-    curl_setopt($ch, CURLOPT_URL, "https://api.softwareavanzado.world/index.php?option=com_contact&webserviceVersion=1.0.0&webserviceClient=administrator&filter[search]={$filter_string}&api=Hal");
+    curl_setopt($ch, CURLOPT_URL, "https://api.softwareavanzado.world/index.php?option=com_contact&webserviceVersion=1.0.0&webserviceClient=administrator&filter[search]={$filter_string}&list[limit]=100&api=Hal");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    //curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
     $response = curl_exec($ch);
     curl_close($ch);
 
     $json = json_decode($response);
-    print_r($response);
     return $json->_embedded->item;
 }
 
@@ -77,28 +76,63 @@ function list_contact_filter($filter_string)
 function print_list_contacts($array_list_contacts)
 {
     foreach ($array_list_contacts as $contact) {
-        echo "ID: {$contact->id}, Name Contact: {$contact->name}\n";
+        echo "ID: {$contact->id}, Name Contact: {$contact->name} <br>\n";
     }
 }
 
+
+/*
+*   Obtiene el token mediante la autorización del servidor, validando si el cliente_id y
+*   cliente_secret son validos.
+*   @client_id
+*   @client_secret
+*   @grant_type
+*/
+function get_bearer_token($client_id, $client_secret, $grant_type)
+{
+    $data = [
+        'client_id' => $client_id,
+        'client_secret' => $client_secret,
+        'grant_type' => $grant_type
+    ];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://api.softwareavanzado.world/index.php?option=token&api=oauth2");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $json = json_decode($response);
+    return $json->access_token;
+}
+
+//array de contactos
 $contacts = [
-    "201612460 Contacto 1",
-    "201612460 Contacto 2",
-    "201612460 Contacto 3",
-    "201612460 Contacto 4",
-    "201612460 Contacto 5",
-    "201612460 Contacto 6",
-    "201612460 Contacto 7",
-    "201612460 Contacto 8",
-    "201612460 Contacto 9",
-    "201612460 Contacto 10",
+    "201612460 Contacto 1 Token SA",
+    "201612460 Contacto 2 Token SA",
+    "201612460 Contacto 3 Token SA",
+    "201612460 Contacto 4 Token SA",
+    "201612460 Contacto 5 Token SA",
+    "201612460 Contacto 6 Token SA",
+    "201612460 Contacto 7 Token SA",
+    "201612460 Contacto 8 Token SA",
+    "201612460 Contacto 9 Token SA",
+    "201612460 Contacto 10 Token SA",
 ];
 
 /*
-*	Si se desea crear mas contactos modificar $contacts, con los contactos a crear
-* 	y descomentar, o mandar a llamar a la funcion create_contacts, la cual se encargara
+*	Si se desea crear mas contactos modificar $contacts, con los contactos a crear,
+* 	y mandar a llamar a la funcion create_contacts, la cual se encargara
 * 	crear los contactos
 */
-//create_contacts($contacts);
 
-print_list_contacts(list_contact_filter("201612460"));
+//llamada para obtener el token
+$token = get_bearer_token($client_id, $client_secret, $grant_type);
+//imprimir titulo y llamar a la funcion de crear contactos
+echo "<h1>Creación de contactos</h1> <br>\n";
+create_contacts($contacts, $token);
+//imprimir titulo y llamar a la funcion de imprimir una lista de contactos
+echo "<h1>Lista de contactos</h1> <br>\n";
+print_list_contacts(list_contact_filter("201612460", $token));
--- README.md
+++ README.md
@@ -1,8 +1,25 @@
-# Tarea 1 - Software Avanzado
+# Practica 2 - Software Avanzado
 
 Esta aplicación es un cliente que consume el web service: https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=hal&format=doc.
 
-Realizando las siguientes operaciones:
+###### La parte 1 consiste en:
+Continuación de la pratica 1, agregando credenciales tipo client_credentials y un token Bearer para poder volver a desarrollar el mismo ejercicio.
+
+En donde el flujo consiste, en solicitar al servidor un bearer token utilizando credenciales tipo client_credentials, para luego con el bearer token realizar los request corresopndientes para la creación de contactos y listar los mismos.
+
+Realizando las siguientes operaciones para este caso:
+  - Crear contacto
+  - Crear multiples contactos
+  - Listar los contactos por medio de un filtro de tipo texto
+  - Imprimir la lista de contactos
+  - Obtener bearer token 
+
+###### La parte 2 consiste en:
+Continuación de la pratica 1, ahora utilizando SOAP y autenticación básica.
+
+El flujo consiste en realizar el request al servidor utilizando autentificación basica y por medio del cuerpo del request enviar el xml con los parametros encargado para realizar el metodo correspondiente del lado del servidor.
+
+Realizando las siguientes operaciones para este caso:
   - Crear contacto
   - Crear multiples contactos
   - Listar los contactos por medio de un filtro de tipo texto
@@ -19,3 +36,5 @@ $ apt-get install php
 ```sh
 $ php SATarea1.php
 ```
+###### NOTA:
+También puede ser ejecutado desde un navegador.
